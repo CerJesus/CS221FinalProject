a1cc8ccc27e0efb218607c6f19f73a95b31b0e33
Alex Kim <alexkim7@stanford.edu>
2018-05-25 17:58:39 -0700

    created util.py to take care of redundant functions; fully integrated util.py into baseline.py; mostly integrated util.py into regression.py; also general code cleaning and comments

--- baseline.py
+++ baseline.py
@@ -6,66 +6,53 @@ Authors:  Alexandre Bucquet, Jesus Cervantes, Alex Kim
 Python 2.7
 
 DESCRIPTION
-This script evaluates the accuracy of a naive baseline predictor
+This script defines a naive baseline predictor and evaluates its mean squared
+error.
 """
 
 import math, random
 from collections import defaultdict
 import numpy  as np
 import pandas as pd
+from util import evaluatePredictor, csvAsArray
 
-# FUNCTIONS --------------------------------------------------------------------
+# LEARNING FUNCTIONS -----------------------------------------------------------
 
 # BASELINE: Return the median sale price given an appropriate input tuple
 def learnBaseline(np_array):
-	sale_prices = [np_array[i][80] for i in range(len(np_array))]
-	np.sort(sale_prices)
-	median_sale_price = sale_prices[len(sale_prices) / 2 - 1] / 1000.0
+    sale_prices = [np_array[i][80] for i in range(len(np_array))]
+    np.sort(sale_prices)
+    median_sale_price = sale_prices[len(sale_prices) / 2 - 1] / 1000.0
 
-	def baselinePredictor(input):
-		return median_sale_price
+    def baselinePredictor(input):
+	return median_sale_price
 
-	return baselinePredictor
+    return baselinePredictor
 
 # ORACLE: Return the true sale price given an appropriate input tuple
 def learnOracle(examples):
-	examples_map = {}
-	for input, output in examples:
-		examples_map[input] = output
+    examples_map = {}
+    for input, output in examples:
+	examples_map[input] = output
 
-	def predictor(x):
-		return examples_map[x]
+    def predictor(x):
+	return examples_map[x]
 
-	return predictor
-
-# PREDICTOR EVALUATION: Compute the mean squared error of a predictor function
-# given a matrix of examples
-def evaluatePredictor(predictor, examples):
-	error = 0.0
-	for input, output in examples:
-		error += (predictor(input) - output) ** 2
-	error /= len(examples)
-	return error
+    return predictor
 
 # COMPUTATION ------------------------------------------------------------------
 
-# Import the training data into a numpy matrix
-file_train  = 'train.csv'
-data_train  = pd.read_csv(file_train)
-train_array = data_train.as_matrix(columns = None)
-
-# Import the test data into a numpy matrix
-file_test  = 'test.csv'
-data_test  = pd.read_csv(file_test)
-test_array = data_test.as_matrix(columns=None)
+# Import the training and test data as numpy matrices
+train_array = csvAsArray('train.csv')
+test_array  = csvAsArray('test.csv')
 
 # Format the training data as a list of (input, output) tuples
 train_examples = []
 for i in range(len(train_array)):
-	input_size = range(len(train_array[i]) - 1)
-	input      = (train_array[i][j] for j in input_size)
-	output	   = train_array[i][80] / 1000.0
-	train_examples.append((input, output))
+    input_size = range(len(train_array[i]) - 1)
+    input_data = (train_array[i][j] for j in input_size)
+    output     = train_array[i][80] / 1000.0
+    train_examples.append((input_data, output))
 
 # Define predictor functions for baseline and oracle
 baseline     = learnBaseline(train_array)
@@ -77,9 +64,9 @@ oracle_error   = evaluatePredictor(oracle_train, train_examples)
 
 # Print the results
 print ""
-print "--------"
-print "BASELINE"
-print "--------"
+print "-------------------"
+print "BASELINE AND ORACLE"
+print "-------------------"
 print "Number of examples:    ", len(train_examples)
 print "Baseline (median) MSE: ", baseline_error
 print "Oracle MSE:            ", oracle_error
---
+++ util.py
@@ -0,0 +1,52 @@
+"""
+Beating the Bubble: Housing Prices in Ames, Iowa
+
+Filename: util.py
+Authors:  Alexandre Bucquet, Jesus Cervantes, Alex Kim
+Python 2.7
+
+DESCRIPTION
+Common functions
+"""
+
+import math, random
+from collections import defaultdict
+import numpy  as np
+import pandas as pd
+
+# VECTOR FUNCTIONS -------------------------------------------------------------
+
+# DOT PRODUCT: Compute the dot product of two numpy vectors
+def dotProduct(vec1, vec2):
+    dot_product = 0
+    for key, value in vec1.iteritems():
+	if key in vec2.keys():
+	    dot_product += vec1[key] * vec2[key]
+    return dot_product
+
+# INCREMENT: Increment a vector (vec1 += scale * vec2)
+def increment(vec1, scale, vec2):
+    for key, value in vec2.items():
+	if key in vec1.keys():
+	    vec1[key] += scale * vec2[key]
+	else:
+	    vec1[key] = scale * vec2[key]
+
+# PREDICTOR FUNCTIONS ----------------------------------------------------------
+
+# PREDICTOR EVALUATION: Compute the mean squared error of a predictor function
+# given a matrix of examples
+def evaluatePredictor(predictor, examples):
+    error = 0.0
+    for input, output in examples:
+	error += (predictor(input) - output) ** 2
+    error /= len(examples)
+    return error
+
+# DATA IMPORT ------------------------------------------------------------------
+
+# IMPORT DATA: Takes the filename of a csv and returns a numpy array
+def csvAsArray(data_file):
+    data       = pd.read_csv(data_file, header=0)
+    data_array = data.values
+    return data_array
